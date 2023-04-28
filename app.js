89fe9806b1fb8570633af4fd05beed76e1cfa137
Vinicius Jesus <92672912+viniciusantos2105@users.noreply.github.com>
2023-04-28 12:50:31 -0300

    atualizando rotas

--- app.js
+++ app.js
@@ -5,6 +5,7 @@
     const bodyParser = require("body-parser");
     const app = express()
     const admin = require("./routes/admin")
+    const path = require("path")
     //const mongoose = require("mongoose");
 
 //Configurações
@@ -15,7 +16,18 @@
         app.engine('handlebars', handle.engine);
         app.set('view engine', 'handlebars');
     //Mongoose
+    
+    //
+        //app.use(express.static(path.join(__dirname + "/public")))
 //Rotas
+    app.get('/', (req, res) => {
+        res.send('Rota principal')
+    })
+
+    app.get('/posts', (req, res) => {
+        res.send("Lista Posts")
+    })
+
     app.use('/admin', admin)
 //Outros
 const Port = 8081
