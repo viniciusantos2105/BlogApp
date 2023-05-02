5f22856b96fdb3c455f242094adf462d740e9731
Vinicius Jesus <92672912+viniciusantos2105@users.noreply.github.com>
2023-05-02 15:54:30 -0300

    Validando formularios

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 //Carregando módulos
-    const express = require('express');
+    const express = require('express')
     const handlebars = require('express-handlebars')
-    var handle = handlebars.create({defaultLayout: 'main'});
-    const bodyParser = require("body-parser");
+    var handle = handlebars.create({defaultLayout: 'main'})
+    const bodyParser = require("body-parser")
     const app = express()
     const admin = require("./routes/admin")
     const path = require("path")
-    const mongoose = require("mongoose");
-
+    const mongoose = require("mongoose")
+    const session = require("express-session")
+    const flash = require("connect-flash")
 //Configurações
+    //Sessão
+        app.use(session({
+            secret: "appblog",
+            resave: true,
+            saveUninitialized: true
+        }))
+        app.use(flash())
+    //Middleware
+        app.use((req, res, next) =>{
+            res.locals.success_msg = req.flash("success_msg")
+            res.locals.error_msg = req.flash("error_msg")
+            next()
+        })    
     //Body Parser
         app.use(bodyParser.urlencoded({extended: true}))
         app.use(bodyParser.json())
@@ -26,11 +40,7 @@
             console.log("Houve um erro ao se conectar ao mongoDB: "+ err)
         })
     // Public
-        //app.use(express.static(path.join(__dirname + "/public")))
-        app.use((req, res, next) =>{
-            console.log("Eu sou um middlewere")
-            next()
-        })
+        //app.use(express.static(path.join(__dirname + "/public"))
 //Rotas
     app.get('/', (req, res) => {
         res.send('Rota principal')
