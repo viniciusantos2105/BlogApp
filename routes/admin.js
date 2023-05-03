8b525cbea126aa1bf8eae13f8763735a647c58eb
Vinicius Jesus <92672912+viniciusantos2105@users.noreply.github.com>
2023-05-03 08:33:29 -0300

    implementando formulario de postagens

--- routes/admin.js
+++ routes/admin.js
@@ -111,4 +111,17 @@ router.post("/categorias/deletar", (req, res) =>{
     })
 })
 
+router.get("/postagens", (req, res) =>{
+    res.render("admin/postagens")
+})
+
+router.get("/postagens/add", (req, res) =>{
+    Categoria.find().lean().then((categorias) =>{
+        res.render("admin/addpostagens", {categorias: categorias})
+    }).catch((err) =>{
+        req.flash("error_msg", "Houve um erro ao carregar formulário")
+        res.redirect("/admin")
+    })
+})
+
 module.exports = router
