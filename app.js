a4e122c1e889283aaf3ef55751f40707b2aea1fc
Vinicius Jesus <92672912+viniciusantos2105@users.noreply.github.com>
2023-05-03 20:12:52 -0300

    aplicando listagem de categorias

--- app.js
+++ app.js
@@ -11,6 +11,8 @@
     const flash = require("connect-flash")
     require("./models/Postagem")
     const Postagem = mongoose.model("postagens")
+    require("./models/Categoria")
+    const Categoria = mongoose.model("categorias")
 //Configurações
     //Sessão
         app.use(session({
@@ -64,6 +66,37 @@
             }
         }).catch((err) =>{
             req.flash("error_msg", "Houve um erro interno")
+            res.redirect("/")
+        })
+    })
+
+    app.get("/categorias", (req, res)=>{
+        Categoria.find().lean().then((categorias) =>{
+            res.render("categorias/index", {categorias: categorias})
+        }).catch((err)=>{
+            req.flash("error_msg", "Houve um erro interno ao listar categorias")
+            res.redirect("/")
+        })
+    })
+
+    app.get("/categorias/:slug", (req, res) =>{
+        Categoria.findOne({slug: req.params.slug}).lean().then((categoria) =>{
+            if(categoria){
+                Postagem.find({categoria: categoria._id}).lean().then((postagens) =>{
+
+                    res.render("categorias/postagens", {postagens: postagens, categoria: categoria})
+
+                }).catch((err) =>{
+                    req.flash("error_msg", "Houve um erro ao listar os posts")
+                    res.redirect("/")
+                })
+            }
+            else{
+                req.flash("error_msg", "Esta categoria não existe")
+                res.redirect("/")
+            }
+        }).catch((err) =>{
+            req.flash("error_msg", "Houve um erro interno ao carregar a página desta categoria")
             res.redirect("/")
         })
     })
