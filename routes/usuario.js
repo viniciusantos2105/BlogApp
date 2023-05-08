2fc199d25622289f9c8d32c89ddd1c9e3698f8f5
Vinicius Jesus <92672912+viniciusantos2105@users.noreply.github.com>
2023-05-08 10:50:24 -0300

    add bcrypt e implementando login e registro de usuarios

--- routes/usuario.js
+++ routes/usuario.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const mongoose = require('mongoose')
 require("../models/Usuario")
 const Usuario = mongoose.model("usuarios")
+const bcrypt = require("bcryptjs")
 
 router.get("/registro", (req, res)=>{
     res.render("usuarios/registro")
@@ -35,6 +36,49 @@ router.post("/registro", (req, res)=>{
 
         res.render("usuarios/registro", {erros: erros})
     }
+    else{
+        Usuario.findOne({email: req.body.email}).then((usuario)=>{
+            if(usuario){
+                req.flash("error_msg", "Já existe uma conta com este email")
+                res.redirect("/usuarios/registro")
+            }
+            else{
+
+                const novoUsuario = new Usuario({
+                    nome: req.body.nome,
+                    email: req.body.email,
+                    senha: req.body.senha
+                })
+
+                bcrypt.genSalt(10, (erro, salt) =>{
+                    bcrypt.hash(novoUsuario.senha, salt, (erro, hash) =>{
+                        if(erro){
+                            req.flash("error_msg", "Houve um erro ao salvar usuario")
+                            res.redirect("/")
+                        }
+
+                        novoUsuario.senha = hash
+
+                        novoUsuario.save().then(() =>{
+                            req.flash("success_msg", "Usuario criado com sucesso")
+                            res.redirect("/")
+                        }).catch((err) =>{
+                            req.flash("error_msg", "Houve um erro ao criar usuario")
+                            res.redirect("/usuarios/registro")
+                        })
+                    })
+                })
+
+            }
+        }).catch((err) =>{
+            req.flash("error_msg", "Houve um erro interno")
+            res.redirect("/")
+        })
+    }
+})
+
+router.get("/login", (req, res) =>{
+    res.render("usuarios/login")
 })
 
 module.exports = router
