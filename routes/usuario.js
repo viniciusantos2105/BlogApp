799a8f42525fc6744cc84792c49ea24ecd6ddb01
Vinicius Jesus <92672912+viniciusantos2105@users.noreply.github.com>
2023-05-08 13:25:29 -0300

    implementando função de logout

--- routes/usuario.js
+++ routes/usuario.js
@@ -90,4 +90,12 @@ router.post("/login", (req, res, next) =>{
     })(req, res, next)
 })
 
+router.get('/logout', (req, res, next) => {
+    req.logout(function(err) {
+        if (err) { return next(err) }
+        req.flash("success_msg", "Deslogado com sucesso")
+        res.redirect('/')
+      })
+})
+
 module.exports = router
